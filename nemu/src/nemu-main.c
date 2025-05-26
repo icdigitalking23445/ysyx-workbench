da672d8fcd48e66d91710542e139604907df19da
weiyaoli <weiyaoleevv@gmail.com>
2025-05-26 21:32:33 +0800

    PA2.1-complished

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -19,6 +19,8 @@ void init_monitor(int, char *[]);
 void am_init_monitor();
 void engine_start();
 int is_exit_status_bad();
+//typedef unsigned int word_t;
+//word_t expr(char *e, bool *success);
 
 int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
@@ -27,7 +29,49 @@ int main(int argc, char *argv[]) {
 #else
   init_monitor(argc, argv);
 #endif
+/*
+  FILE *fp = fopen("input", "r");
+  if (fp == NULL) {
+    perror("Failed to open input file");
+    return 1;
+  }
 
+  char line[65536];
+  int line_no = 0;
+  int pass = 0;
+  int total = 0;
+
+  while (fgets(line, sizeof(line), fp)) {
+    line_no++;
+
+    // 解析每一行：格式为 <期望结果> <表达式>
+    unsigned expected = 0;
+    char expr_buf[65536];
+    if (sscanf(line, "%u %[^\n]", &expected, expr_buf) != 2) {
+      printf("Line %d: Bad format: %s", line_no, line);
+      continue;
+    }
+
+    bool success = true;
+    unsigned actual = expr(expr_buf, &success);
+
+    if (!success) {
+      printf("Line %d:  Evaluate failed: %s\n", line_no, expr_buf);
+    } else if (actual != expected) {
+      printf("Line %d: Wrong: expect %u, got %u, expr: %s\n",
+             line_no, expected, actual, expr_buf);
+    } else {
+      pass++;
+    }
+
+    total++; */
+ // }
+
+ // fclose(fp);
+
+ // printf("Test Summary: %d / %d passed\n", pass, total);
+
+  //return 0;
   /* Start engine. */
  	engine_start();
   return is_exit_status_bad();
